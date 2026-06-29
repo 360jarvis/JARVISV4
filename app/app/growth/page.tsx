21c291a0e593a97e4be9e75921f2779986fb4488
Flix091 <info@camilipro.com>
2026-06-29 10:50:26 -0500

    Connect Growth route

---
+++ app/app/growth/page.tsx
@@ -0,0 +1,6 @@
+import AppShell from '../../../components/AppShell';
+import GrowthCommandCenter from '../../../components/GrowthCommandCenter';
+
+export default function GrowthPage() {
+  return <AppShell title="Growth" subtitle="Leads, follow-up, reviews, referrals, and attribution."><GrowthCommandCenter /></AppShell>;
+}
