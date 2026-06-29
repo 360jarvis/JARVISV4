2479b4434777c626a9f2a40268c7f943cc218612
Flix091 <info@camilipro.com>
2026-06-29 08:58:48 -0500

    Add Financial Command Center route

---
+++ app/app/financial/page.tsx
@@ -0,0 +1,6 @@
+import AppShell from '../../../components/AppShell';
+import ModulePlaceholder from '../../../components/ModulePlaceholder';
+
+export default function FinancialPage() {
+  return <AppShell title="Financial Command Center" subtitle="Revenue, open balances, payroll costs, inventory costs, refunds, credits, and QuickBooks-style reporting."><ModulePlaceholder name="Financial Command Center" /></AppShell>;
+}
