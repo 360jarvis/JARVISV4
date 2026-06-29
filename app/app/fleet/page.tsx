efa3c8b06df95279f2c6099361df6d8e729ce7a7
Flix091 <info@camilipro.com>
2026-06-29 11:07:45 -0500

    Connect Fleet route

---
+++ app/app/fleet/page.tsx
@@ -0,0 +1,6 @@
+import AppShell from '../../../components/AppShell';
+import SimpleFleetCenter from '../../../components/SimpleFleetCenter';
+
+export default function FleetPage() {
+  return <AppShell title="Fleet" subtitle="Vehicles, service tickets, assignments, and reminders."><SimpleFleetCenter /></AppShell>;
+}
