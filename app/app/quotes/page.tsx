3a3be37f7fa4afb323fe6b8648f17ed24936bf10
Flix091 <info@camilipro.com>
2026-06-29 09:16:04 -0500

    Add Quotes route

---
+++ app/app/quotes/page.tsx
@@ -0,0 +1,6 @@
+import AppShell from '../../../components/AppShell';
+import QuoteBuilder from '../../../components/QuoteBuilder';
+
+export default function QuotesPage() {
+  return <AppShell title="Quotes" subtitle="Create, track, and convert quotes using the shared Smart Pricing Engine."><QuoteBuilder /></AppShell>;
+}
