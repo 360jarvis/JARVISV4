1933b5da09a79896b900acb3540cabff8641467d
Flix091 <info@camilipro.com>
2026-06-29 11:12:00 -0500

    Connect Support route

---
+++ app/app/support/page.tsx
@@ -0,0 +1,6 @@
+import AppShell from '../../../components/AppShell';
+import SupportCenter from '../../../components/SupportCenter';
+
+export default function SupportPage() {
+  return <AppShell title="Support" subtitle="Workspace help, setup guidance, platform notices, and assistance."><SupportCenter /></AppShell>;
+}
