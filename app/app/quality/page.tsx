d18751682eae0bee9f660f4bc6ee51f2f8e5060e
Flix091 <info@camilipro.com>
2026-06-29 10:50:03 -0500

    Connect Quality route to Command Center

---
+++ app/app/quality/page.tsx
@@ -0,0 +1,6 @@
+import AppShell from '../../../components/AppShell';
+import QualityCommandCenter from '../../../components/QualityCommandCenter';
+
+export default function QualityPage() {
+  return <AppShell title="Quality Command Center" subtitle="Inspections, complaints, re-cleans, damage claims, reviews, and quality risk."><QualityCommandCenter /></AppShell>;
+}
