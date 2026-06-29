cbfe7a8ee5642898544bb68f74603f4826b3ef01
Flix091 <info@camilipro.com>
2026-06-29 08:58:22 -0500

    Add Workforce Command Center route

---
+++ app/app/workforce/page.tsx
@@ -0,0 +1,6 @@
+import AppShell from '../../../components/AppShell';
+import ModulePlaceholder from '../../../components/ModulePlaceholder';
+
+export default function WorkforcePage() {
+  return <AppShell title="Workforce Command Center" subtitle="Providers, employees, schedules, clock events, payroll setup, documents, and compliance."><ModulePlaceholder name="Workforce Command Center" /></AppShell>;
+}
