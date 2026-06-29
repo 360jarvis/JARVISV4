a3e82dac00e8c8984c5bb986a72fc9c2dcb20238
Flix091 <info@camilipro.com>
2026-06-29 11:13:17 -0500

    Connect Settings route

---
+++ app/app/settings/page.tsx
@@ -0,0 +1,6 @@
+import AppShell from '../../../components/AppShell';
+import SettingsCenter from '../../../components/SettingsCenter';
+
+export default function SettingsPage() {
+  return <AppShell title="Settings" subtitle="Company setup, branding, business rules, payments, notifications, users, and portal controls."><SettingsCenter /></AppShell>;
+}
