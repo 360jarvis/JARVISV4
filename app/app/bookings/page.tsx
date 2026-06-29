c3f0efb4799de79f1139c5871884eb556307b28b
Flix091 <info@camilipro.com>
2026-06-29 08:56:31 -0500

    Add Booking Command Center route

---
+++ app/app/bookings/page.tsx
@@ -0,0 +1,6 @@
+import AppShell from '../../../components/AppShell';
+import ModulePlaceholder from '../../../components/ModulePlaceholder';
+
+export default function BookingsPage() {
+  return <AppShell title="Booking Command Center" subtitle="Scheduled, recurring, quote-backed, canceled, and unassigned booking operations."><ModulePlaceholder name="Booking Command Center" /></AppShell>;
+}
