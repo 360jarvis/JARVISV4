7226fe329bb6d027afb9d27a1651aed10c77d9a3
Flix091 <info@camilipro.com>
2026-06-29 08:58:12 -0500

    Add Dispatch route

---
+++ app/app/dispatch/page.tsx
@@ -0,0 +1,6 @@
+import AppShell from '../../../components/AppShell';
+import ModulePlaceholder from '../../../components/ModulePlaceholder';
+
+export default function DispatchPage() {
+  return <AppShell title="Dispatch" subtitle="Schedule operations, assignments, maps, and route visibility."><ModulePlaceholder name="Dispatch" /></AppShell>;
+}
