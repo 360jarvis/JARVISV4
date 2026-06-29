929c4da5a756a47647f6e429bf8ae59c34044797
Flix091 <info@camilipro.com>
2026-06-29 08:56:12 -0500

    Add Customer Command Center route

---
+++ app/app/customers/page.tsx
@@ -0,0 +1,6 @@
+import AppShell from '../../../components/AppShell';
+import ModulePlaceholder from '../../../components/ModulePlaceholder';
+
+export default function CustomersPage() {
+  return <AppShell title="Customer Command Center" subtitle="Customer records, notes, bookings, quotes, documents, reviews, and timeline."><ModulePlaceholder name="Customer Command Center" /></AppShell>;
+}
